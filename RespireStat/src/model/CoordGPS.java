6d2914c92e821980536a2030f0f414e065df5940
Kuros <Kuros@LAPTOP-MCB2LJS7>
2019-04-11 17:57:30 +0200

    CoordGPS

--- RespireStat/src/model/CoordGPS.java
+++ RespireStat/src/model/CoordGPS.java
@@ -1,5 +1,15 @@
 package model;
 
 public class CoordGPS {
-
+	private double latittude;
+	private double longitude;
+	
+	public coordGPS(String) {
+		
+	}
+	
+	public coordGPS(double lattitude , double longitude) {
+		this.lattitude = lattitude;
+		this.longitude = longitude;
+	}
 }

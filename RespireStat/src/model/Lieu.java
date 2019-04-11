096a7e430e14bc250a873ff2e771d1615cc9195e
Kuros <Kuros@LAPTOP-MCB2LJS7>
2019-04-11 17:47:28 +0200

    Lieu remplie

--- RespireStat/src/model/Lieu.java
+++ RespireStat/src/model/Lieu.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Lieu {
-
+	private String codePostal;
+	private String departement;
+	private String ville;
+	
+	
+	public Lieu (String codePostal , String departement , String ville) {
+		this.codePostal = codePostal;
+		this.departement = departement;
+		this.ville = ville;
+	}
 }

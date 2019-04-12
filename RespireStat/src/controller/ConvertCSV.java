9c403f2c624e93f2149796d4fb41ce70c1012c7d
f.chlakhoff <f.chlakhoff@gmail.com>
2019-04-12 10:00:39 +0200

    Mise à jour

--- RespireStat/src/controller/ConvertCSV.java
+++ RespireStat/src/controller/ConvertCSV.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class ConvertCSV {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
 
-}
+public class ConvertCSV {
+	
+	public static void lire (String fileName) {
+	
+		try {
+			CSVReader reader = new CSVReader (new FileReader("fileName"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+			
+		}
+	}
+
--- RespireStat/src/vue/RespireStat.java
+++ RespireStat/src/vue/RespireStat.java
@@ -1,5 +1,9 @@
 package vue;
+import controller.ConvertCSV;
 
 public class RespireStat {
+	public static void lire (String fileName) {
+		
+	}
 	
 }
